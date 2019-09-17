95b0a8f294c82d0c5d53913d3103bd24530483bd
Denn Orina <anyoksdenn@gmail.com>
2019-09-17 11:26:06 +0300

    added seeds

--- distro/db/seeds.rb
+++ distro/db/seeds.rb
@@ -1,3 +1,4 @@
+require 'csv'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,105 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#   Scheme seeds
+# ['Ruiru', 'Jacaranda', 'Juja'].each do |schm|
+# 	if Scheme.find_by_name("#{schm}").present?
+# 		puts "SCH EXISTS #{schm}----------Skipping"
+# 	else
+#         Scheme.find_or_create_by({name: schm, description: "Water Treatment company"})
+#  	end
+# end
+
+# Zone
+
+# ['21 Ruiru'].each do |zone|
+#     if Zone.find_by_name("#{zone}").present?
+#         puts "SCH EXISTS #{zone}----------Skipping"        
+#     else
+#         Zone.find_or_create_by({name: zone, description: "Water Treatment company"})
+#     end
+
+# end
+
+# Sub Zone
+# ['006 Thome'].each do |subzone|
+#     if Subzone.find_by_name("#{subzone}").present?
+#         puts "SCH EXISTS #{subzone}----------Skipping"        
+#     else
+#         Subzone.find_or_create_by({name: subzone, description: "Water Treatment company"})
+#         puts "SUBZONE #{subzone}----------Skipping" 
+#     end
+
+# end
+
+# stages
+
+# ['Assign', 'Further Action', 'Pending', 'Complete'].each do |stage|
+#     if Stage.find_by_name("#{stage}").present?
+#         puts "STAGE EXISTS #{stage}----------Skipping"        
+#     else
+#         Stage.find_or_create_by({name: stage, description: "This is a stage desc"})
+#         puts "Stage #{stage}----------SAVED" 
+#     end
+# end
+
+# Position
+['Billing', 'Technician' ].each do |stage|
+    if Position.find_by_name("#{stage}").present?
+        puts "STAGE EXISTS #{stage}----------Skipping"        
+    else
+        Position.find_or_create_by({name: stage, description: "This is a stage desc"})
+        puts "Stage #{stage}----------SAVED" 
+    end
+end
+
+
+# csv_text = File.read(Rails.root.join('lib', 'seeds', 'Accounts.csv'))
+# # display the wall of text
+# # puts csv_text  
+# csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+# csv.each do |row|
+#   #  Put s the hash to be save
+# #   puts row.to_hash
+#   if Walkroute.find_by(name: "#{row['Walk Route']}")
+#     puts "WALK ROUTE  EXISTS number #{row['Walk Route']}----------Skipping"
+#   else
+#     Walkroute.create!(name: "#{row['Walk Route']}", subzone_id: Subzone.first.id)
+#     puts "WALK ROUTE CREATED #{row['Route']}----------SAVED"
+#   end
+# end
+  
+# csv_text = File.read(Rails.root.join('lib', 'seeds', 'Accounts.csv'))
+# # display the wall of text
+# # puts csv_text  
+# csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+
+# csv.each do |row|
+
+#     if Account.find_by(number: "#{row['Account']}")
+#         puts "ACCOUNT EXISTS number #{row['Account']}----------Skipping"
+#     else
+#         # Create a new Obeject 
+#         number        = row['Account']
+#         meterSerial   = row['Mtr Serial']
+#         name          = row['Account']
+#         walkroute_id  = Walkroute.where( name: row['Walk Route']).first.id
+#         address       =  "default"
+#         latitude      = row['Lat']
+#         longitude     = row['Long']      
+
+#         account = Account.create!( number: number, meterSerial: meterSerial, name: name, walkroute_id: walkroute_id, address: address, latitude: latitude, longitude: longitude)
+        
+#         # Save the obeject
+#         if account
+#             puts "#{account.number} ==> saved"
+#         else
+#             puts "#{account.number} ERR:: Failed to Save! "
+#             puts "\n"
+#             puts "****Error****"
+#             puts "#{account.errors.messages}"
+#         end
+#     end
+#   end
+
