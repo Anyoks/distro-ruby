10afba64ee1d6ff7f1943ae22fb4f01a440fa983
Denn Orina <anyoksdenn@gmail.com>
2019-10-27 16:28:28 +0000

    change user

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :stage, :production
 set :rails_env, :production
-server '46.101.122.43', user: 'distro', roles: %w{web app db}
+server '46.101.122.43', user: 'deploy', roles: %w{web app db}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
