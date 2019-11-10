102ac0ac135735cf588421eb9505c5f30ea2b08c
Denn Orina <anyoksdenn@gmail.com>
2019-11-10 07:50:18 +0300

    more query examples

--- app/gqlqueries.rb
+++ app/gqlqueries.rb
@@ -25,6 +25,27 @@ query {
  }
 }
 
+query($userId: String!) {
+    myassignments(userId: $userId) {
+      id
+      stageName
+      account {
+        number
+        name
+      }
+      staff {
+        firstName
+        lastName
+      }
+      task {
+        name
+      }
+      report {
+        comments
+      }
+    }
+  }
+
 # my tasks (from my department)
 query {   
   mytasks(
@@ -66,4 +87,56 @@ query {
 	 name
    myTotalAssignments(userId:"9676bf1e-acce-4b97-a0cf-3028df2e76f0" ) 
  }
+}
+
+# My report
+query($userId: String!) {
+    myreports(userId: $userId) {
+    id
+    comments
+    completed
+    furtherAction{
+      name
+      other{
+        name
+      }
+    }
+    
+    }
+  }
+# All reports
+query {
+  	reports {
+    id
+    comments
+    completed
+    furtherAction{
+      name
+      other{
+        name
+      }
+    }
+    
+    }
+  }
+
+#########################################################################
+#####################   MUTATIONS  ######################################
+#########################################################################
+
+# create report
+mutation{
+  createReport(
+  input:{
+    completed:true,
+    comments:"no isses",
+    furtherActionId: "06723a70-c4d8-4fbc-97e8-cb368c1267f3",
+    assignmentId: "00ab8a26-d26c-4a4d-bbd6-d7bd437e1b56" ,
+    otherComment: ""
+  }
+  ){
+    report{
+      id
+    }
+  }
 }
