cf4943b8b398b252d142fda5dc08f603777c6795
Denn Orina <anyoksdenn@gmail.com>
2019-11-11 04:46:12 +0000

    account number is null able

--- app/graphql/types/account_type.rb
+++ app/graphql/types/account_type.rb
@@ -2,7 +2,7 @@ module Types
   class AccountType < Types::BaseObject
     field :id, String, null: false
     field :name, String, null: true
-    field :number, String, null: false
+    field :number, String, null: true
     field :old_account_number, String, null: true
     field :meterSerial, String, null: true
     field :walkroute, Types::WalkrouteType, null: true
