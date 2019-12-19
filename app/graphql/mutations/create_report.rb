2aceaf78096fa3652d142edd0ce079f791a98218
Denn Orina <anyoksdenn@gmail.com>
2019-12-19 13:24:08 +0300

    updated the create report mutation

--- app/graphql/mutations/create_report.rb
+++ app/graphql/mutations/create_report.rb
@@ -9,97 +9,64 @@ module Mutations
     argument :further_action_id, String, required: true
     argument :assignment_id, String, required: true
     argument :other_comment, String, required: false
-    # argument :picture, String, required: false # base 64 string
-    # argument :file, ApolloUploadServer::Upload, required: false
+
+    argument :remark_id, String, required: true
+    argument :meter_serial, String, required: false
+    argument :meter_reading, String, required: false
+    argument :other_remark, String, required: false
 
     field :report, Types::ReportType, null: false
     field :errors, [String], null: false
     field :other, String, null: true
+    field :other_remark, String, null:true
 
     
-    def resolve(completed:,comments:,further_action_id:,assignment_id:, other_comment:)
-      report = Report.new(completed: completed ,comments: comments,further_action_id: further_action_id,assignment_id: assignment_id)
+    def resolve(completed:,comments:,further_action_id:,assignment_id:,other_comment:,remark_id:,meter_reading:,meter_serial:,other_remark:)
+      
+      report = Report.new(completed: completed ,comments: comments,further_action_id: further_action_id,assignment_id: assignment_id,remark_id: remark_id,meter_reading: meter_reading,meter_serial: meter_serial)
       
       if report.save
-        # uncomment the line below on update of the new apk
-        # upload = upload_picture(report, picture)
-        upload = true
-
-        # byebug
-        if upload
+           
           
-          if other_comment.length != 0
-            Other.create!(name: other_comment, further_action_id: further_action_id)
+        if (other_comment.length != 0 && other_remark.length != 0)
+          Other.create!(name: other_comment, further_action_id: further_action_id)
+          OtherRemark.create!(name: other_remark, remark_id: remark_id )
             # Successful creation, return the created object with no errors
+          {
+            report: report,
+            other: 'yes',
+            other_remark: 'yes',
+            errors: [],
+          }
+        elsif other_comment.length != 0
+          Other.create!(name: other_comment, further_action_id: further_action_id)
+          {
+            report: report,
+            other: 'yes',
+            other_remark: 'no',
+            errors: [],
+          }
+        elsif other_remark.length != 0
+          OtherRemark.create!(name: other_remark, remark_id: remark_id )
             {
               report: report,
-              other: 'yes',
-              picture: 'yes',
+              other: 'no',
+              other_remark: 'yes',
               errors: [],
             }
-          else
-            {
-              report: report,
-              other: nil,
-              picture: 'yes',
-              errors: [report.errors.full_messages],
-            }
-           end
-        else
-          {
-              report: report,
-              other: nil,
-              picture: nil,
-              errors: [upload],
-          }
-        end
+        end        
         
       else
         # Failed save, return the errors to the client
         # raise GraphQL::ExecutionError, report.errors.full_messages.join(", ")
+        {
+          report: nil,
+          other: 'no',
+          other_remark: 'no',
+          errors: [report.errors.full_messages],
+        }
       end
       # raise GraphQL::ExecutionError, report.errors.full_messages.join(", ")
-    ensure
-      # makes sure the temp files are cleaned
-      clean_tempfile 
-    end
-
-    def upload_picture(report, picture)
-      filename = report.assignment.staff.first_name
-
-        @tempfile = Tempfile.new(filename)
-        @tempfile.binmode
-        @tempfile.write Base64.decode64(picture)
-        @tempfile.rewind
-        content_type = `file --mime -b #{@tempfile.path}`.split(";")[0]
-        extension = content_type.match(/gif|jpeg|png/).to_s
-        
-        @tempfile2 = Tempfile.new([filename, ".#{extension}"])
-        @tempfile2.binmode
-        @tempfile2.write Base64.decode64(picture)
-        @tempfile2.rewind
-        # byebug
-        pic = report.pictures.create!(picture: @tempfile2 )
-        if pic
-          return true
-        else
-          return pic.errors.full_messages
-        end
-
-        # byebug
-    end
-
-    def clean_tempfile
-      Rails.logger.info("Cleaning Temp files")
-      if @tempfile
-        @tempfile.close
-        @tempfile.unlink
-      end
-
-      if @tempfile2
-        @tempfile2.close
-        @tempfile2.unlink
-      end
     end
   end
 end
