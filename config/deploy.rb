a6f015b449f0468f8a898fb7a0a12b42fca5eef9
Denn Orina <anyoksdenn@gmail.com>
2020-02-06 16:05:21 +0300

    shared

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,7 +20,7 @@ set :deploy_to, "/home/deploy/distro"
 # set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
 # 
 set :bundle_binstubs, nil
-set :linked_files, %w{config/database.yml config/secrets.yml config/initializers/devise.rb config/initializers/apartment.rb config/master.key config/credentials.yml.enc }
+set :linked_files, %w{config/database.yml config/secrets.yml config/initializers/devise.rb config/initializers/apartment.rb config/master.key }
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/images}
 
 # cap production deploy:upload_yml
@@ -29,10 +29,10 @@ namespace :deploy do
     task :upload_yml do
       on roles(:app) do
         execute "mkdir #{shared_path}/config -p"
-        upload! StringIO.new(File.read("config/database.yml")), "#{shared_path}/config/database.yml"
-        upload! StringIO.new(File.read("config/secrets.yml")), "#{shared_path}/config/secrets.yml"
-        upload! StringIO.new(File.read("config/initializers/apartment.rb")), "#{shared_path}/config/initializers/apartment.rb"
-        upload! StringIO.new(File.read("config/credentials.yml.enc")), "#{shared_path}/config/credentials.yml.enc"
+        # upload! StringIO.new(File.read("config/database.yml")), "#{shared_path}/config/database.yml"
+        # upload! StringIO.new(File.read("config/secrets.yml")), "#{shared_path}/config/secrets.yml"
+        # upload! StringIO.new(File.read("config/initializers/apartment.rb")), "#{shared_path}/config/initializers/apartment.rb"
+        # upload! StringIO.new(File.read("config/credentials.yml.enc")), "#{shared_path}/config/credentials.yml.enc"
         upload! StringIO.new(File.read("config/master.key")), "#{shared_path}/config/master.key"
         # upload! StringIO.new(File.read("config/initializers/devise.rb}")), "#{shared_path}/config/initializers/devise.rb"
       end
