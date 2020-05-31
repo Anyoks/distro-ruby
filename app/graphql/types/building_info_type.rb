35e8906d960dfcd20648b9e88c3d784901df27d3
Denn Orina <anyoksdenn@gmail.com>
2020-05-31 14:02:06 +0300

    allowed null values

--- app/graphql/types/building_info_type.rb
+++ app/graphql/types/building_info_type.rb
@@ -1,12 +1,12 @@
 module Types
   class BuildingInfoType < Types::BaseObject
-    field :id, String, null: false
-    field :single_rooms,  String, null: false
-    field :bedsitters,  String, null: false
-    field :one_bedrooms,  String, null: false
-    field :two_bedrooms,  String, null: false
-    field :three_bedrooms,  String, null: false
-    field :floor_id,   String, null: false
-    field :connection_info_id, String, null: false
+    field :id, String, null: true
+    field :single_rooms,  String, null: true
+    field :bedsitters,  String, null: true
+    field :one_bedrooms,  String, null: true
+    field :two_bedrooms,  String, null: true
+    field :three_bedrooms,  String, null: true
+    field :floor_id,   String, null: true
+    field :connection_info_id, String, null: true
   end
 end
